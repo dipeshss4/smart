3da099999940f76f59346b8488a1b040a7476335
IT Department <dipesh.ss19@gmail.com>
2019-07-04 17:31:54 +0545

    adding changes

--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -45,29 +45,7 @@
                                 <span class="selected"></span>
                                 <span class="arrow open"></span>
                             </a>
-                            <ul class="sub-menu">
-                                <li class="nav-item start active open">
-                                    <a href="index.html" class="nav-link ">
-                                        <i class="icon-bar-chart"></i>
-                                        <span class="title">Dashboard 1</span>
-                                        <span class="selected"></span>
-                                    </a>
-                                </li>
-                                <li class="nav-item start ">
-                                    <a href="dashboard_2.html" class="nav-link ">
-                                        <i class="icon-bulb"></i>
-                                        <span class="title">Dashboard 2</span>
-                                        <span class="badge badge-success">1</span>
-                                    </a>
-                                </li>
-                                <li class="nav-item start ">
-                                    <a href="dashboard_3.html" class="nav-link ">
-                                        <i class="icon-graph"></i>
-                                        <span class="title">Dashboard 3</span>
-                                        <span class="badge badge-danger">5</span>
-                                    </a>
-                                </li>
-                            </ul>
+                           
                         </li>
                         <li class="heading">
                             <h3 class="uppercase">Features</h3>
